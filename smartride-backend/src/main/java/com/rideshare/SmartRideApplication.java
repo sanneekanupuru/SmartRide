a7bb2334ce3d49a2964b786a893f3c1907a5ec06
sannee <sanneekanupuru@gmail.com>
2025-08-26 17:24:04 +0530

    Updated SmartRideApplication.java

--- smartride-backend/src/main/java/com/rideshare/SmartRideApplication.java
+++ smartride-backend/src/main/java/com/rideshare/SmartRideApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RideshareApplication {
+public class SmartRideApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RideshareApplication.class, args);
+        SpringApplication.run(SmartRideApplication.class, args);
     }
 }
